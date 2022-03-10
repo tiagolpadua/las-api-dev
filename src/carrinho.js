be9d4405715fa3e4393cb5a1a0997bf7c67b238c
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2022-03-10 17:58:34 -0300

    testes carrinho

--- src/carrinho.js
+++ src/carrinho.js
@@ -1,110 +1,124 @@
-const readline = require("readline");
 const {
   listarProdutos,
   listarCategorias,
   listarCuponsValidos,
 } = require("./api-service");
+const { askQuestion } = require("./ask-question");
 const { formatarValor } = require("./objetos");
 
-function askQuestion(query) {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  return new Promise((resolve) =>
-    rl.question(query, (ans) => {
-      rl.close();
-      resolve(ans);
-    })
-  );
-}
-
 let opcao = "";
+let produtos;
+let categorias;
+let cupons;
+let carrinho = [];
 
-async function run() {
-  const carrinho = [];
-  let produtos = await listarProdutos();
-  const categorias = await listarCategorias();
+async function carregarDadosDaAPI() {
+  produtos = await listarProdutos();
+  categorias = await listarCategorias();
+  cupons = await listarCuponsValidos();
+
   produtos = produtos.map((p) => {
     const categoria = categorias.find((c) => p.categoria === c.nome);
     const produto = { ...p, desconto: categoria?.desconto || 0 };
     return produto;
   });
-  const cupons = await listarCuponsValidos();
+}
+
+async function run() {
+  carregarDadosDaAPI();
 
   do {
-    console.log("Escolha uma opção:");
-    console.log("1 - Listar produtos");
-    console.log("2 - Incluir produto no carrinho");
-    console.log("3 - Visualizar carrinho");
-    console.log("4 - Finalizar compra");
-    console.log("x - Sair");
+    imprimirOpcoes();
     opcao = await askQuestion("Opção: ");
-
-    if (opcao === "1") {
-      console.log("Lista de Produtos:");
-      console.table(produtos);
-    } else if (opcao === "2") {
-      let codProduto = await askQuestion(
-        "Qual código de produto deseja incluir no carrinho?"
-      );
-      codProduto = parseInt(codProduto);
-      const produto = produtos[codProduto];
-      if (!produto) {
-        console.error(`Produto não localizado: ${codProduto}`);
-        break;
-      }
-      let qtd = await askQuestion(
-        "Quantas unidades deseja adicionar ao carrinho?"
-      );
-      qtd = parseInt(qtd);
-      if (!qtd) {
-        console.error(`Quantidade inválida: ${qtd}`);
-        break;
-      }
-      let item = carrinho.find((i) => i.id === codProduto);
-      if (item) {
-        item.qtd = item.qtd + qtd;
-        item.valor = item.preco * item.qtd;
-        item.valor = item.valor - item.valor * (item.desconto / 100);
-      } else {
-        item = { id: codProduto, ...produto, qtd, valor: produto.preco * qtd };
-        item.valor = item.valor - item.valor * (item.desconto / 100);
-        carrinho.push(item);
-      }
-      console.log("Produto incluído com sucesso no carrinho.");
-    } else if (opcao === "3") {
-      console.log("Carrinho de Compras:");
-      console.table(carrinho);
-    } else if (opcao === "4") {
-      console.log("Concluir compra:");
-      console.table(carrinho);
-      const subtotal = carrinho.reduce((v, i) => i.valor + v, 0);
-      console.log(`Subtotal: ${formatarValor(subtotal)}`);
-      let cupom = await askQuestion("Qual o nome do cupom?");
-      let descontoCupom = 0;
-      if (cupom) {
-        if (!cupons.find((c) => c === cupom)) {
-          console.error("Cupom inválido");
-          break;
-        }
-
-        descontoCupom = await askQuestion("Qual o desconto do cupom?");
-        descontoCupom = parseInt(descontoCupom);
-      }
-      const total = subtotal - subtotal * (descontoCupom / 100);
-      console.log(`Total: ${formatarValor(total)}`);
-      console.log("Compra finalizada com sucesso!");
-    } else if (opcao === "x") {
-      console.log("Adeus...");
-    } else {
-      console.log(`Opção inválida: ${opcao}`);
-    }
-    console.log("\n");
+    processarOpcao(opcao);
   } while (opcao !== "x");
+}
+
+function imprimirOpcoes() {
+  console.log("Escolha uma opção:");
+  console.log("1 - Listar produtos");
+  console.log("2 - Incluir produto no carrinho");
+  console.log("3 - Visualizar carrinho");
+  console.log("4 - Finalizar compra");
+  console.log("x - Sair");
+}
+
+async function processarOpcao(opcao) {
+  if (opcao === "1") {
+    console.log("Lista de Produtos:");
+    console.table(produtos);
+  } else if (opcao === "2") {
+    let codProduto = await askQuestion(
+      "Qual código de produto deseja incluir no carrinho?"
+    );
+    codProduto = parseInt(codProduto);
+    const produto = produtos[codProduto];
+    if (!produto) {
+      console.error(`Produto não localizado: ${codProduto}`);
+      return;
+    }
+    let qtd = await askQuestion(
+      "Quantas unidades deseja adicionar ao carrinho?"
+    );
+    qtd = parseInt(qtd);
+    if (!qtd) {
+      console.error(`Quantidade inválida: ${qtd}`);
+      return;
+    }
+    let item = carrinho.find((i) => i.id === codProduto);
+    if (item) {
+      item.qtd = item.qtd + qtd;
+      item.valor = item.preco * item.qtd;
+      item.valor = item.valor - item.valor * (item.desconto / 100);
+    } else {
+      item = { id: codProduto, ...produto, qtd, valor: produto.preco * qtd };
+      item.valor = item.valor - item.valor * (item.desconto / 100);
+      carrinho.push(item);
+    }
+    console.log("Produto incluído com sucesso no carrinho.");
+  } else if (opcao === "3") {
+    console.log("Carrinho de Compras:");
+    console.table(carrinho);
+  } else if (opcao === "4") {
+    console.log("Concluir compra:");
+    console.table(carrinho);
+    const subtotal = carrinho.reduce((v, i) => i.valor + v, 0);
+    console.log(`Subtotal: ${formatarValor(subtotal)}`);
+    let cupom = await askQuestion("Qual o nome do cupom?");
+    let descontoCupom = 0;
+    if (cupom) {
+      if (!cupons.find((c) => c === cupom)) {
+        console.error("Cupom inválido");
+        return;
+      }
+
+      descontoCupom = await askQuestion("Qual o desconto do cupom?");
+      descontoCupom = parseInt(descontoCupom);
+    }
+    const total = subtotal - subtotal * (descontoCupom / 100);
+    console.log(`Total: ${formatarValor(total)}`);
+    console.log("Compra finalizada com sucesso!");
+  } else if (opcao === "x") {
+    console.log("Adeus...");
+  } else {
+    console.error(`Opção inválida: ${opcao}`);
+  }
+  console.log("\n");
 }
 
 if (require.main === module) {
   run();
 }
+
+function esvaziarCarrinho() {
+  carrinho = [];
+}
+
+module.exports = {
+  run,
+  esvaziarCarrinho,
+  askQuestion,
+  imprimirOpcoes,
+  processarOpcao,
+  carregarDadosDaAPI,
+};
--- src/carrinho.test.js
+++ src/carrinho.test.js
@@ -1,3 +1,26 @@
+const {
+  listarProdutos,
+  listarCategorias,
+  listarCuponsValidos,
+} = require("./api-service");
+
+const { askQuestion } = require("./ask-question");
+
+jest.mock("./api-service");
+jest.mock("./ask-question");
+
+const {
+  imprimirOpcoes,
+  carregarDadosDaAPI,
+  processarOpcao,
+  run,
+  esvaziarCarrinho,
+} = require("./carrinho");
+
+const PRODUTOS_MOCK = require("../mocks/produtos.json");
+const CATEGORIAS_MOCK = require("../mocks/categorias.json");
+const CUPONS_MOCK = require("../mocks/cupons.json");
+
 describe("Desafio", () => {
   // Crie uma interface com testes em linha de comandos que:
   // 1 - Liste os produtos
@@ -5,4 +28,136 @@ describe("Desafio", () => {
   // 3 - Visualize o carrinho
   // 4 - Finalize a compra e pergunte pelo cupom de desconto
   // x - Saia do sistema
+
+  beforeEach(() => {
+    console.log = jest.fn();
+    console.table = jest.fn();
+    console.error = jest.fn();
+    listarProdutos.mockResolvedValue(PRODUTOS_MOCK);
+    listarCategorias.mockResolvedValue(CATEGORIAS_MOCK);
+    listarCuponsValidos.mockResolvedValue(CUPONS_MOCK);
+    carregarDadosDaAPI();
+    esvaziarCarrinho();
+    askQuestion.mockClear();
+  });
+
+  test("Deve imprimir as opções.", () => {
+    imprimirOpcoes();
+    expect(console.log.mock.calls).toEqual([
+      ["Escolha uma opção:"],
+      ["1 - Listar produtos"],
+      ["2 - Incluir produto no carrinho"],
+      ["3 - Visualizar carrinho"],
+      ["4 - Finalizar compra"],
+      ["x - Sair"],
+    ]);
+  });
+
+  test("Deve sair se informar x.", async () => {
+    askQuestion.mockResolvedValue("x");
+    await run();
+    expect(JSON.stringify(console.log.mock.calls)).toEqual(
+      '[["Escolha uma opção:"],["1 - Listar produtos"],["2 - Incluir produto no carrinho"],["3 - Visualizar carrinho"],["4 - Finalizar compra"],["x - Sair"],["Adeus..."],["\\n"]]'
+    );
+  });
+
+  test("Deve recusar uma opção inválida.", async () => {
+    processarOpcao("999");
+    expect(JSON.stringify(console.error.mock.calls)).toEqual(
+      '[["Opção inválida: 999"]]'
+    );
+  });
+
+  test("Deve listar os produtos com a opção 1.", async () => {
+    processarOpcao("1");
+    expect(JSON.stringify(console.log.mock.calls)).toEqual(
+      '[["Lista de Produtos:"],["\\n"]]'
+    );
+    expect(JSON.stringify(console.table.mock.calls)).toEqual(
+      '[[[{"nome":"Confete","categoria":"Infantil","preco":30,"desconto":30},{"nome":"Serpentina","categoria":"Infantil","preco":10,"desconto":30},{"nome":"Cerveja","categoria":"Bebida","preco":7,"desconto":0},{"nome":"Refrigerante","categoria":"Bebida","preco":8,"desconto":0},{"nome":"Fruta","categoria":"Alimentação","preco":12,"desconto":15}]]]'
+    );
+  });
+
+  test("Não deve incluir um produto no carrinho com a opção 2 se o código for inválido.", async () => {
+    askQuestion.mockResolvedValue("999999");
+    await processarOpcao("2");
+    expect(JSON.stringify(console.error.mock.calls)).toEqual(
+      '[["Produto não localizado: 999999"]]'
+    );
+  });
+
+  test("Não deve incluir um produto no carrinho com a opção 2 se a quantidade for inválida.", async () => {
+    askQuestion.mockResolvedValueOnce("1");
+    askQuestion.mockResolvedValueOnce("a");
+    await processarOpcao("2");
+    expect(JSON.stringify(console.error.mock.calls)).toEqual(
+      '[["Quantidade inválida: NaN"]]'
+    );
+  });
+
+  test("Deve incluir um produto no carrinho com a opção 2 se o código do produto e a quantidade estiverem corretos.", async () => {
+    askQuestion.mockResolvedValueOnce("1");
+    askQuestion.mockResolvedValueOnce("2");
+    await processarOpcao("2");
+    expect(JSON.stringify(console.log.mock.calls)).toEqual(
+      '[["Produto incluído com sucesso no carrinho."],["\\n"]]'
+    );
+
+    askQuestion.mockResolvedValueOnce("1");
+    askQuestion.mockResolvedValueOnce("2");
+    await processarOpcao("2");
+
+    expect(JSON.stringify(console.log.mock.calls)).toEqual(
+      '[["Produto incluído com sucesso no carrinho."],["\\n"],["Produto incluído com sucesso no carrinho."],["\\n"]]'
+    );
+  });
+
+  test("Deve imprimir o carrinho com a opção 3.", async () => {
+    askQuestion.mockResolvedValueOnce("1");
+    askQuestion.mockResolvedValueOnce("2");
+    askQuestion.mockResolvedValueOnce("3");
+
+    await processarOpcao("2");
+    await processarOpcao("3");
+
+    expect(JSON.stringify(console.log.mock.calls)).toEqual(
+      '[["Produto incluído com sucesso no carrinho."],["\\n"],["Carrinho de Compras:"],["\\n"]]'
+    );
+
+    expect(JSON.stringify(console.table.mock.calls)).toEqual(
+      '[[[{"id":1,"nome":"Serpentina","categoria":"Infantil","preco":10,"desconto":30,"qtd":2,"valor":14}]]]'
+    );
+  });
+
+  test("Deve concluir a compra com a opção 4.", async () => {
+    askQuestion.mockResolvedValueOnce("1");
+    askQuestion.mockResolvedValueOnce("ALURANUX");
+    askQuestion.mockResolvedValueOnce("10");
+
+    await processarOpcao("2");
+
+    await processarOpcao("4");
+
+    expect(JSON.stringify(console.table.mock.calls)).toEqual(
+      '[[[{"id":3,"nome":"Refrigerante","categoria":"Bebida","preco":8,"desconto":0,"qtd":1,"valor":8}]]]'
+    );
+
+    expect(JSON.stringify(console.log.mock.calls)).toEqual(
+      '[["Produto incluído com sucesso no carrinho."],["\\n"],["Concluir compra:"],["Subtotal: R$ 8,00"],["Total: R$ 7,20"],["Compra finalizada com sucesso!"],["\\n"]]'
+    );
+  });
+
+  test("Deve recusar a compra com a opção 4 com cupom inválido.", async () => {
+    askQuestion.mockResolvedValueOnce("1");
+    askQuestion.mockResolvedValueOnce("2");
+    askQuestion.mockResolvedValueOnce("CUPOM-INVALIDO");
+
+    await processarOpcao("2");
+
+    await processarOpcao("4");
+
+    expect(JSON.stringify(console.error.mock.calls)).toEqual(
+      '[["Cupom inválido"]]'
+    );
+  });
 });
