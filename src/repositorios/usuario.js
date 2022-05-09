4326cf20dd5743374b7c27a2e28e761c4925cf28
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2022-05-09 17:48:32 -0300

    semana 22

--- src/repositorios/usuario.js
+++ src/repositorios/usuario.js
@@ -2,12 +2,12 @@ const query = require("../infraestrutura/database/queries");
 
 class Usuario {
   listar() {
-    const sql = "SELECT * FROM Usuarios";
+    const sql = "SELECT id, nome, urlFotoPerfil FROM Usuarios ";
     return query(sql);
   }
 
   buscarPorId(id) {
-    const sql = "SELECT * FROM Usuarios WHERE id = ?";
+    const sql = "SELECT id, nome, urlFotoPerfil FROM Usuarios WHERE id = ?";
     return query(sql, id);
   }
 
@@ -27,8 +27,47 @@ class Usuario {
   }
 
   buscarPorNome(nome) {
-    const sql = "SELECT * FROM Usuarios WHERE nome like ?";
+    const sql =
+      "SELECT id, nome, urlFotoPerfil FROM Usuarios WHERE nome like ?";
     return query(sql, "%" + nome + "%");
+  }
+
+  atualizarDadosPessoais(id, dadosPessoais) {
+    const sql = "UPDATE Usuarios SET ? WHERE id = ?";
+    return query(sql, [dadosPessoais, id]);
+  }
+
+  obterDadosPessoais(id) {
+    const sql =
+      "SELECT nomeCompleto, dataNascimento, rg, cpf FROM Usuarios WHERE id = ?";
+    return query(sql, id);
+  }
+
+  atualizarContatos(id, dadosPessoais) {
+    const sql = "UPDATE Usuarios SET ? WHERE id = ?";
+    return query(sql, [dadosPessoais, id]);
+  }
+
+  obterContatos(id) {
+    const sql =
+      "SELECT nomeCompleto, dataNascimento, rg, cpf FROM Usuarios WHERE id = ?";
+    return query(sql, id);
+  }
+
+  atualizarSenha(id, dadosPessoais) {
+    const sql = "UPDATE Usuarios SET ? WHERE id = ?";
+    return query(sql, [dadosPessoais, id]);
+  }
+
+  atualizarEndereco(id, dadosPessoais) {
+    const sql = "UPDATE Usuarios SET ? WHERE id = ?";
+    return query(sql, [dadosPessoais, id]);
+  }
+
+  obterEndereco(id) {
+    const sql =
+      "SELECT nomeCompleto, dataNascimento, rg, cpf FROM Usuarios WHERE id = ?";
+    return query(sql, id);
   }
 }
 
